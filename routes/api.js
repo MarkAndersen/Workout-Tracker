d351f41e67f44b2741d6bb0eae7c4ab546e80603
Mark Andersen <10101087+MarkAndersen@users.noreply.github.com>
2021-08-08 17:57:20 -0400

    testing out aggregator with routing

--- routes/api.js
+++ routes/api.js
@@ -23,6 +23,19 @@ router.get("/api/workouts", async (req, res) => {
   }
 });
 
+//below is an aggregator, will be likely what we use
+
+// router.get("/api/workouts", async (req, res) => {
+//     try {
+//       const getAllWorkouts = await Workout.aggregate([{ 
+//           $group: { day: {$dateToString: { format: "%Y-%M-%D", date: "$date"}}},
+//           $addFields: { totalDuration: {$sum: "$duration"} } } ]);
+//       res.status(200).json(getAllWorkouts);
+//     } catch (err) {
+//       res.status(400).json(err);
+//     }
+//   });
+
 router.get("/api/workouts/:id", async (req, res) => {
   try {
     const oneWorkout = await Workout.findById(req.params.id);
@@ -33,7 +46,6 @@ router.get("/api/workouts/:id", async (req, res) => {
   }
 });
 
-//updates but doesn't add all fields...
 router.put("/api/workouts/:id", async (req, res) => {
   try {
     const updateWorkout = await Workout.findByIdAndUpdate(
