9b30d2d4eb47c2f84c2a8d958397822940fc875a
Mark Andersen <10101087+MarkAndersen@users.noreply.github.com>
2021-08-10 22:14:50 -0400

    almost finished with get routes. need to limit and sort /ranges and should be finished

--- routes/api.js
+++ routes/api.js
@@ -14,37 +14,50 @@ router.post("/api/workouts", async ({ body }, res) => {
 });
 
 //DB seeded, gets all workouts
-router.get("/api/workouts", async (req, res) => {
-  try {
-    const getAllWorkouts = await Workout.find({});
-    res.status(200).json(getAllWorkouts);
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
+// router.get("/api/workouts", async (req, res) => {
+//   try {
+//     const getAllWorkouts = await Workout.find({});
+//     res.status(200).json(getAllWorkouts);
+//   } catch (err) {
+//     res.status(400).json(err);
+//   }
+// });
 
 //below is an aggregator, will be likely what we use
 
-// router.get("/api/workouts", async (req, res) => {
-//     try {
-//       const getAllWorkouts = await Workout.aggregate([{ 
-//           $group: { day: {$dateToString: { format: "%Y-%M-%D", date: "$date"}}},
-//           $addFields: { totalDuration: {$sum: "$duration"} } } ]);
-//       res.status(200).json(getAllWorkouts);
-//     } catch (err) {
-//       res.status(400).json(err);
-//     }
-//   });
+router.get("/api/workouts", async (req, res) => {
+    try {
+      const getAllWorkouts = await Workout.aggregate([{ 
+          // $group: { day: {$dateToString: { format: "%Y-%M-%D", date: "$date"}}},
+          $addFields: { totalDuration: {$sum: "$exercises.duration"} } } ]);
+      res.status(200).json(getAllWorkouts);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
 
-router.get("/api/workouts/:id", async (req, res) => {
-  try {
-    const oneWorkout = await Workout.findById(req.params.id);
+router.get("/api/workouts/range", async (req, res) => {
+    try {
+      const getAllWorkouts = await Workout.aggregate([{ 
+          // $group: { day: {$dateToString: { format: "%Y-%M-%D", date: "$date"}}},
+          $addFields: { totalDuration: {$sum: "$exercises.duration"} } } ])
+          //sort
+          //limit to 7;
+      res.status(200).json(getAllWorkouts);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
 
-    res.json(oneWorkout);
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
+// router.get("/api/workouts/:id", async (req, res) => {
+//   try {
+//     const oneWorkout = await Workout.findById(req.params.id);
+
+//     res.json(oneWorkout);
+//   } catch (err) {
+//     res.status(400).json(err);
+//   }
+// });
 
 router.put("/api/workouts/:id", async (req, res) => {
   try {
