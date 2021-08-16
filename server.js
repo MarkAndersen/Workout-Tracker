10f1a6484e5222bf932bb377f693cb4990c28726
Mark Andersen <10101087+MarkAndersen@users.noreply.github.com>
2021-08-16 19:50:52 -0400

    fixed type in require

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const logger = require("morgan");
-const path = requite("path");
+const path = require("path");
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3000;
 
