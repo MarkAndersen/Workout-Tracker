6ccfbcb97b825d39155d9d09f827b752b78be670
Mark Andersen <10101087+MarkAndersen@users.noreply.github.com>
2021-08-16 19:54:30 -0400

    removing dependency

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const logger = require("morgan");
-const path = require("path");
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3000;
 
