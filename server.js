9d40b5ab667c71ebdffaccf9c51ea82a97152c3b
Mark Andersen <10101087+MarkAndersen@users.noreply.github.com>
2021-08-19 21:59:34 -0400

    fixing deploy

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ app.use(express.static("public"));
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useNewUrlParser: true,
   useFindAndModify: false
 });
 
